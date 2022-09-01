800566ff31e26044424e70ffbf915a72218eab5d
navayuvan-avasoft <navayuvan.s@avasoft.com>
2022-09-01 22:13:51 +0530

    Communicate lambda

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,54 @@ import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import { commonTagsController } from "./controllers/commonTags.controller";
 import serverless from "serverless-http";
+import runMiddleware from "run-middleware";
 
 var cors = require("cors");
 
 const app = express();
+runMiddleware(app);
+
 app.use(json());
 app.use(cors());
 
 require("dotenv").config();
+
+app.use(
+  "/todos/*/functions/TodosFunction/invocations",
+  (req: Request, res: Response) => {
+    const payload = JSON.parse(Buffer.from(req.body).toString());
+    (app as any).runMiddleware(
+      payload.path,
+      {
+        method: payload.httpMethod,
+        body: payload.body,
+        query: payload.queryParams,
+      },
+      function (code: any, data: any) {
+        res.json(data);
+      }
+    );
+  }
+);
 
 app.get("/", (req: Request, res: Response) => {
   return res.send("Todo service is healthy!");
 });
 
 app.get("/todos/check", (req, res) => {
-  return res.send("Todo service is working fine");
+  return res.send({ message: "Attendance Service is working fine" });
+});
+
+app.get("/todos/attendance", (req, res) => {
+  return res.send({ message: "Todo Service is working fine" });
+});
+
+app.get("/todos/mobile", (req, res) => {
+  return res.send({ message: "Mobile Service is working fine" });
+});
+
+app.get("/todos/users", (req, res) => {
+  return res.send({ message: "Users Service is working fine" });
 });
 
 app.use(
